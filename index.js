4a193483e8c9bebf1861105bf383bcae4b56d467
krishna saparia <krishnasaparia43045@gmail.com>
2020-10-19 12:15:23 -0400

    add nodemon and setup server with GET and POST method

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req , res) =>{
-    res.send('Hello World!')
+    res.send("You're at Set on home page")
 })
 
+app.get('/clean', (req , res) =>{
+    res.send("Erased everything...")
+})
+
+app.post('/' , (req , res) =>{
+    res.send("Nothing is posted")
+})
+ 
 app.listen(port,() =>{
-    console.log('app listening at http://localhost:${port}')
+    console.log(`app listening at http://localhost: ${port}`)
 })
